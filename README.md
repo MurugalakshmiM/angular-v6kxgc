02fc422060bb44f8bb59d85e8077ddf16f429602
MurugalakshmiM <mmurugalakshmi.03@gmail.com>
2019-11-07 15:12:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-v6kxgc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-v6kxgc)
---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent implements OnInit {
+  name = 'Angular';
+
+  ngOnInit(){
+    
+  }
+
+}
